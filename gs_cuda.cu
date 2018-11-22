8d72cf934ce032cff369b451184d67ff992caa46
Silvia Barbero <100316961@alumnos.uc3m.es>
2018-11-22 16:27:09 +0100

    Aproximacion del solver y comentarios del profe
    Primera idea sobre el calculo de la matriz ya aplanada y teniendo en cuenta los threads, comentarios que ha ido diciendo el profe en clase para que los tengamos en cuenta a la hora de programar y medidas de tiempo y escritura en ficheros en funcion de lo que se pide en el enunciado con la libreria time.h

--- gs_cuda.cu
+++ gs_cuda.cu
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 
 #define MAX_ITER 100
@@ -49,33 +50,45 @@ void allocate_dev_matrix(float **mat, int n, int m) {
 
 
 // Solver (TODO)
-__global__ void solver(float **mat, int n, int m) {
+__global__ void solver(float **mat, int n, int m, int num_elems) {
 
 	float diff = 0, temp;
 	int done = 0, cnt_iter = 0, i, j;
 
-	while (!done && (cnt_iter < MAX_ITER)) {
+	while (!threadIdx.done && (threadIdx.cnt_iter < MAX_ITER)) {
 		diff = 0;
-
+		
+		// No termino de tener muy claro que esto sea asi, pero :D
 		for (i = 1; i < n - 1; i++) {
+  			for (j = 1; j < m - 1; j++) {
+				(*mat)[threadIdx.i*m + threadIdx.j] = 0.2 * ((*mat)[threadIdx.i*m + threadIdx.j] + 
+									     (*mat)[threadIdx.i*m + (threadIdx.j - 1)] + 
+									     (*mat)[(threadIdx.i - 1)*m + threadIdx.j] + 
+									     (*mat)[threadIdx.i*m + (threadIdx.j + 1)] + 
+									     (*mat)[(threadIdx.i + 1)*m + threadIdx.j]);
+				threadIdx.diff += abs((*mat)[threadIdx.i*m + threadIdx.j] - threadIdx.temp);
+		
+			}
+		}
+		/*for (i = 1; i < n - 1; i++) {
 			for (j = 1; j < m - 1; j++) {
 				temp = (*mat)[i][j];
 				(*mat)[i][j] = 0.2 * ((*mat)[i][j] + (*mat)[i][j - 1] + (*mat)[i - 1][j] + (*mat)[i][j + 1] + (*mat)[i + 1][j]);
 				diff += abs((*mat)[i][j] - temp);
 			}
-		}
+		}*/
 
-		if (diff/n/n < TOL) {
-			done = 1; 
+		if (threadIdx.diff/n/n < TOL) {
+			threadIdx.done = 1; 
 		}
-		cnt_iter ++;
+		threadIdx.cnt_iter ++;
 	}
 
-	if (done) {
-		printf("Solver converged after %d iterations\n", cnt_iter);
+	if (threadIdx.done) {
+		printf("Solver converged after %d iterations\n", threadIdx.cnt_iter);
 	}
 	else {
-		printf("Solver not converged after %d iterations\n", cnt_iter);
+		printf("Solver not converged after %d iterations\n", threadIdx.cnt_iter);
 	}
 }
 
@@ -83,7 +96,10 @@ __global__ void solver(float **mat, int n, int m) {
 
 
 int main(int argc, char *argv[]) {
-
+	// Primera medida sobre el tiempo total
+	clock_t ttime, extime;
+	ttime = clock();
+	
 	int n, communication;
 	float *host_mat_org, *host_mat_dest, *dev_matrix;
 
@@ -110,7 +126,20 @@ int main(int argc, char *argv[]) {
 	cudaMemcpy(dev_mat, host_mat_org, mem_size, cudaMemcpyHostToDevice);
 
 
-	/*
+	/*	
+	//////////////////////// COSAS QUE HA DICHO EL PROFE ////////////////////////
+	// Dejar el tamaño del bloque fijo.
+		No se pueden crear bloques mayores a 1024x1024
+		Para hacer pruebas, cambiar el numero de bloques para ver como varian los resultados
+			Para hacer esto esta bien parametrizarlo para recibirlo como input a la hora de hacer las pruebas
+			Para evitar tener que estar cambiando el fichero en cada ejecucion y eso.
+		Como nos dijo el otro dia, intentar siempre que sea divisible entre 32
+		Al depurar el kernel no se pueden poner prints para depurar, lo quitaron a partir de la version 8. 
+			Consejo: tener un vector de control que se pueda copiar al host para poder trazar errores y esas cosas.
+		Si no converge, se puede cambiar el factor TOL para que converja, lo interesante es ver que hace CUDA no que 
+		fufe el algoritmo perfectamente con una convergencia que cuadre.
+	//////////////////////////////////////////////////////////////////////////////
+	
 	// define grid and block size
 	int numBlocks = 8;
 	int numThreadsPerBlock = 8;
@@ -118,8 +147,15 @@ int main(int argc, char *argv[]) {
 	// Configure and launch kernel
 	dim3 dimGrid();
 	dim3 dimBlock();
-	solver<<<dimGrid,dimBlock>>>(&dev_mat, n, n);
-
+	
+	// Medida sobre el tiempo de ejecucion
+	extime = clock();
+	
+	solver<<<dimGrid,dimBlock>>>(&dev_mat, n, n, num_elems);
+	
+	// Fin del tiempo de ejecucion
+	extime = clock() - extime;
+	
 	// block until the device has completed
 	cudaThreadSynchronize();
 	*/
@@ -132,6 +168,26 @@ int main(int argc, char *argv[]) {
 	cudaFree(dev_mat);
 	free(host_mat_org);
 	free(host_mat_dest);
+	
+	// Fin del tiempo total
+	ttime = clock() - ttime;
+	
+	//Mensaje final e imprimir en fichero
+	printf("Total time: %f\n", ttime);
+	printf("Operations time: %f\n", extime);
 
+	FILE *f;
+	if (access("results.csv", F_OK) == -1) {
+ 		f = fopen("results.csv", "a");
+		fprintf(f, "Matrix size;Block size;Total time;Size;Operations time;\n");
+	}
+	else {
+		f = fopen("results.csv", "a");
+	}
+
+	fprintf(f, "%d;%d;%f;%f;\n", n, dimBlock(), ttime, extime);
+	fclose(f);
+
+	
 	return 0;
 }
