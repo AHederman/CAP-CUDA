ef474ac2e526a617120680fa4b04f554fff58141
SILVIA BARBERO RODRIGUEZ <a0316961@lab.inf.uc3m.es>
2018-12-19 16:41:43 +0100

    script para ejecutar

---
+++ exec_script.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+module load cuda/8.0
+
+block_sizes=(16 32 64 128 256)
+matrix_sizes=(2050 4098 8198)
+
+for block_size in "${block_sizes[@]}"
+do
+    echo "-------------------------------"
+    echo "|  Execution mode $block_size  |"
+    echo "-------------------------------"
+
+	for size in "${matrix_sizes[@]}"
+        do
+            echo "-------------------------------"
+            echo "|       Matrix size $size     |"
+            echo "-------------------------------"
+	    ./gs_cuda $size $block_size
+    done
+done
